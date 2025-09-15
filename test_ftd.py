e14c90d3f205d626c605b1d2ac2c656fcc805b49
stormcat1228-afk <stormcat1228@gmail.com>
2025-09-15 18:58:03 -0400

    Update test_ftd.py

--- test_ftd.py
+++ test_ftd.py
@@ -1,10 +1,9 @@
 import sys, os
-# Make Python see the ./src folder
+# Make Python see the ./src folder so we can import ftd_model
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from ftd_model import select_ftd_candidates, format_ftd_message
 
 if __name__ == "__main__":
-    candidates = select_ftd_candidates()
-    print(format_ftd_message(candidates))
-
+    cands = select_ftd_candidates()
+    print(format_ftd_message(cands))
